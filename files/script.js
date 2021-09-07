31172ad61322fd25c4d0b98bb983f1139cbd2a34
mrgokal <87154665+mrgokal@users.noreply.github.com>
2021-09-07 17:37:33 -0500

    Add files via upload
    1st Commit

---
+++ files/script.js
@@ -0,0 +1,19 @@
+function bigImg(x) {
+    x.style.height = "10%";
+    x.style.width = "10%";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "5%";
+    x.style.width = "5%";
+  }
+
+  function bigMypic(x) {
+    x.style.height = "30%";
+    x.style.width = "30%";
+  }
+  
+  function normalMypic(x) {
+    x.style.height = "20%";
+    x.style.width = "20%";
+  }
